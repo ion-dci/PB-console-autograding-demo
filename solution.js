6dbefcfb3bb969acdc69633c435b3da94993e070
ion-dci <ion.lazarev@digitalcareerinstitute.org>
2021-11-17 10:18:33 +0100

    test

--- solution.js
+++ solution.js
@@ -4,11 +4,17 @@ console.log("This is an exercise in console logging");
 let exercise = "This is the value.";
 
 const firstName = "Ion";
+
 const lastName = "Lazarev";
+
 const city = "Berlin";
+
 const job = "Assistant Teacher";
+
 const age = 32;
+
 console.log(firstName + ", " + lastName);
+
 console.log(
   `${firstName} ${lastName} is a ${age} year old ${job} who lives in ${city}.`
 );
